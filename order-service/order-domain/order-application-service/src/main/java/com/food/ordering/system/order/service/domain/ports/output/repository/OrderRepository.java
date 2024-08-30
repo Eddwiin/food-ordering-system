06ecfc7e053fb443f4c06b11739ccabe05badef7
Maguran <maguran.mahendrarajan@gmail.com>
2024-08-30 12:22:04 +0200

    Add domainEventPublisher, input and output for payment

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.order.service.domain.ports.output.repository;
+
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+
+import java.util.Optional;
+
+public interface OrderRepository {
+    Order save(Order save);
+
+    Optional<Order> findByTackingId(TrackingId trackingId);
+
+
+}
