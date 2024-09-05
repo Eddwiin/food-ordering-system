659ca0561a6b7e15e32dda6a05038a6e8e45d859
Maguran <maguran.mahendrarajan@gmail.com>
2024-09-05 11:55:35 +0200

    Add logic for order-dataaccess

--- order-service/order-application/src/main/java/com/food/ordering/system/service/application/rest/OrderController.java
+++ order-service/order-application/src/main/java/com/food/ordering/system/service/application/rest/OrderController.java
@@ -1,6 +1,15 @@
 package com.food.ordering.system.service.application.rest;
 
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -13,9 +22,22 @@ public class OrderController {
         this.orderApplicationService = orderApplicationService;
     }
 
-    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderResponse createOrderResponse) {
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderCommand createOrderCommand) {
+        log.info("Creater order for customer: {} at restaurant {}", createOrderCommand.getCustomerId(),
+                createOrderCommand.getCustomerId());
+        CreateOrderResponse createOrderResponse = orderApplicationService.createOrder(createOrderCommand);
+        log.info("Order created with tracking id: {}", createOrderResponse.getOrderTrackingId());
+        return ResponseEntity.ok(createOrderResponse);
+    }
 
+    @GetMapping("/{tracking}")
+    public ResponseEntity<TrackOrderResponse> getOrderByTracking(@PathVariable UUID trackingId) {
+        TrackOrderResponse trackOrderResponse = orderApplicationService.trackOrder(TrackOrderQuery.builder().orderTrackingId(trackingId).build());
+        log.info("Returning order status with tracking id: {}", trackOrderResponse.getOrderTrackingId());
+        return ResponseEntity.ok(trackOrderResponse);
     }
 }
+
 
 
